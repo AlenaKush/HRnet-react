fd82daec865accd6f01472e6013296b357d2ab49
Kushchina Alena <alenakushchina@gmail.com>
2025-04-27 23:04:44 +0200

    search fixed

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,16 +1,18 @@
-import Form from 'react-bootstrap/Form';
+import { Form } from 'react-bootstrap';
+import InputField from './InputField';
 
 function SearchField({ value, onChange }) {
   return (
     <Form.Group className="mb-3 d-flex align-items-center gap-2 justify-content-end">
-      <Form.Label htmlFor="search" className="mb-0">Search:</Form.Label>
-      <Form.Control
-        id="search"
-        type="text"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        style={{ maxWidth: '250px' }}
-      />
+      <Form.Label htmlFor="search" className="mb-3">Search:</Form.Label>
+      <div style={{ maxWidth: '250px', flex: '1' }}>
+        <InputField
+          name="search"
+          type="text"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      </div>
     </Form.Group>
   );
 }
